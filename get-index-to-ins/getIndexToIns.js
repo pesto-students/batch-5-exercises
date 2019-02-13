89649865e65641cac413d46e10dfe2716a417db4
Ankit307 <bhagatsingh.solanki@appinventiv.com>
2019-02-14 05:06:05 +0530

    Solved get Index

--- get-index-to-ins/getIndexToIns.js
+++ get-index-to-ins/getIndexToIns.js
@@ -1,6 +1,14 @@
-
 function getIndexToIns(...args) {
-  return args;
+  var arrayToSort = args[0].sort()
+  var indexToInsert = 0;
+  for (var i in arrayToSort) {
+    if (i < args[1]) {
+      indexToInsert++;
+    } else {
+      break;
+    }
+  }
+  return arrayToSort;
 }
 
 export {
