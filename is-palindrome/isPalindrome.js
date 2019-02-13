f3c28778543be82329fcd06424cb01a5601a755b
Mehul <Mehul>
2019-02-14 05:01:30 +0530

    WIP : day-3-segment-2, Imp: is-palindrome

--- is-palindrome/isPalindrome.js
+++ is-palindrome/isPalindrome.js
@@ -1,8 +1,19 @@
+function isPalindrome(str) {
+  str = String(str);
+  var filteredStr = '';
+  for (var i = 0; i < str.length; i++) {
+    var char = str[i] + '';
+    var pattern = /[a-zA-Z0-9]/;
+    if (pattern.test(char)) {
+      filteredStr += char;
+    }
+  }
 
-function isPalindrome(...args) {
-  return args;
+  var filterStrReverse = filteredStr
+    .split('')
+    .reverse()
+    .join('');
+  return filterStrReverse.toLowerCase() === filteredStr.toLowerCase();
 }
 
-export {
-  isPalindrome,
-};
+export { isPalindrome };
