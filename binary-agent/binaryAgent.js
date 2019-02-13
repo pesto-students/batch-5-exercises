7f1151079b5744f3380840ac3889c8b0fd6c00c8
Ankit307 <bhagatsingh.solanki@appinventiv.com>
2019-02-14 04:24:57 +0530

    solved binary agent

--- binary-agent/binaryAgent.js
+++ binary-agent/binaryAgent.js
@@ -1,7 +1,11 @@
-
 function binaryAgent(...args) {
-  return args;
-}
+  var binaryNoArray = args[0].split(' ');
+  var resultString = binaryNoArray.map((binaryNo) => {
+    return String.fromCharCode(parseInt(binaryNo, 2));
+  }).join("");
+  return resultString;
+};
+
 
 export {
   binaryAgent,
