5678450e315f3c3252689126140bc69e4d33ca4e
Kaushik Bokka <kaushikbokka@gmail.com>
2019-02-14 04:32:45 +0530

    Solve one exercise

--- sum-all/sumAll.js
+++ sum-all/sumAll.js
@@ -1,6 +1,12 @@
 
-function sumAll(...args) {
-  return args;
+function sumAll(arrayOfTwoNum) {
+  const lowestNumber = Math.min(...arrayOfTwoNum);
+  const biggestNumber = Math.max(...arrayOfTwoNum);
+  let totalSum = 0;
+  for (let i = lowestNumber; i <= biggestNumber; i++) {
+    totalSum += i;
+  }
+  return totalSum;
 }
 
 export {
