3a9e9c4050638dafc1a013570367494b577f6761
Mehul <Mehul>
2019-02-14 04:13:02 +0530

    WIP : day-3-segment-2, Imp: sum-all

--- sum-all/sumAll.js
+++ sum-all/sumAll.js
@@ -1,8 +1,11 @@
-
-function sumAll(...args) {
-  return args;
+function sumAll(range) {
+  var start = Math.min(range[0], range[1]);
+  var end = Math.max(range[0], range[1]);
+  let sum = 0;
+  for (var i = start; i <= end; i++) {
+    sum += i;
+  }
+  return sum;
 }
 
-export {
-  sumAll,
-};
+export { sumAll };
