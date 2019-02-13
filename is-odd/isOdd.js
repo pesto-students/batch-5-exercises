72fff3727736b9fabff3f9864ab9b2463c1b7399
Mehul <Mehul>
2019-02-14 00:21:08 +0530

    WIP : day-3-segment-1, Implement : is-odd

--- is-odd/isOdd.js
+++ is-odd/isOdd.js
@@ -1,8 +1,6 @@
-
-function isOdd(...args) {
-  return args;
+function isOdd(n) {
+  var last = Number(n.toString().slice(-1));
+  return !(last == 0 || last == 2 || last == 4 || last == 6 || last == 8);
 }
 
-export {
-  isOdd,
-};
+export { isOdd };
