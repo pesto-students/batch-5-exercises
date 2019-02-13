76c3598029afb92fddca0c214d479e1632c5ee46
Shalini <shalinityagi64@gmail.com>
2019-02-14 04:59:55 +0530

    Implement getIndexToIns

--- get-index-to-ins/getIndexToIns.js
+++ get-index-to-ins/getIndexToIns.js
@@ -1,6 +1,13 @@
 
-function getIndexToIns(...args) {
-  return args;
+function getIndexToIns(array, value) {
+  array.sort((a, b) => a - b);
+  let i = 0;
+  for (i = 0; i < array.length; i += 1) {
+    if (array[i] >= value) {
+      return i;
+    }
+  }
+  return i;
 }
 
 export {
