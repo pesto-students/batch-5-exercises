490a0d8b0239294b16fd14d90b31895d5682a792
Ankit307 <bhagatsingh.solanki@appinventiv.com>
2019-02-13 23:57:59 +0530

    Solved Apply Operator

--- apply-operator/applyOperator.js
+++ apply-operator/applyOperator.js
@@ -1,8 +1,24 @@
-
 function applyOperator(...args) {
+
+  if (args.length === 1)
+    return 0;
+
+  if (args.length > 0) {
+    var operator = args[0];
+    args = args.slice(1, args.length);
+    if (operator === '-')
+      args = operator + args.join(operator)
+    else
+      args = args.join(operator)
+
+    var result = eval(args)
+    if (operator === '/')
+      result = parseFloat(result.toFixed(4))
+    return result;
+  } else {
+    throw Error('Please define an operator');
+  }
   return args;
 }
 
-export {
-  applyOperator,
-};
+module.exports = applyOperator
