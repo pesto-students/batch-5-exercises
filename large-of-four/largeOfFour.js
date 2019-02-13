2da41027fa15b77a7a4bcc3fd67ee84f5dba5a91
Mehul <Mehul>
2019-02-14 04:48:49 +0530

    WIP : day-3-segment-2, Imp: large-of-four

--- large-of-four/largeOfFour.js
+++ large-of-four/largeOfFour.js
@@ -1,8 +1,14 @@
-
-function largeOfFour(...args) {
-  return args;
+function largeOfFour(arr) {
+  var result = [];
+  for (var i = 0; i < arr.length; i++) {
+    var numArray = arr[i];
+    var maxNum = -Infinity;
+    for (var j = 0; j < numArray.length; j++) {
+      maxNum = Math.max(maxNum, numArray[j]);
+    }
+    result.push(maxNum);
+  }
+  return result;
 }
 
-export {
-  largeOfFour,
-};
+export { largeOfFour };
