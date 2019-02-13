9b0ae13428c18f6570176b012c297f82fe0cce02
Mehul <Mehul>
2019-02-14 00:16:19 +0530

    WIP : day-3-segment-1

--- is-even/isEven.js
+++ is-even/isEven.js
@@ -1,8 +1,6 @@
-
-function isEven(...args) {
-  return args;
+function isEven(n) {
+  var last = Number(n.toString().slice(-1));
+  return last == 0 || last == 2 || last == 4 || last == 6 || last == 8;
 }
 
-export {
-  isEven,
-};
+export { isEven };
