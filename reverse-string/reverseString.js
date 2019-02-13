124a6ea103e45654e4abbb300b5c885bd149c82d
Shalini <shalinityagi64@gmail.com>
2019-02-14 04:30:18 +0530

    Implemented reverseString

--- reverse-string/reverseString.js
+++ reverse-string/reverseString.js
@@ -1,6 +1,14 @@
 
-function reverseString(...args) {
-  return args;
+function reverseString(string) {
+  const array = string.split('');
+  let i = 0;
+  while (i < array.length / 2) {
+    const temp = array[i];
+    array[i] = array[array.length - 1 - i];
+    array[array.length - 1 - i] = temp;
+    i += 1;
+  }
+  return array.join('');
 }
 
 export {
