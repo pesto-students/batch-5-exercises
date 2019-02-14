e9ba3edde400f4a154b85aab98ad7a3b72725220
Ankit307 <bhagatsingh.solanki@appinventiv.com>
2019-02-15 00:37:58 +0530

    Solved map-objects

--- map-object/mapObject.js
+++ map-object/mapObject.js
@@ -1,6 +1,11 @@
-
 function mapObject(...args) {
-  return args;
+  var objectToMap = args[0];
+  var operatingFunction = args[1];
+  var allKeysOfObject = Object.keys(objectToMap);
+  allKeysOfObject.map((key) => {
+    objectToMap[key] = operatingFunction(objectToMap[key]);
+  });
+  return objectToMap;
 }
 
 export {
