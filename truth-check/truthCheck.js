3bd6a37b41eb887c43dc862ff31196136d06556b
Mehul <Mehul>
2019-02-15 00:34:34 +0530

    WIP : day-4-segment-1, Imp : truth-check

--- truth-check/truthCheck.js
+++ truth-check/truthCheck.js
@@ -1,8 +1,11 @@
-
-function truthCheck(...args) {
-  return args;
+function truthCheck(arr, key) {
+  var isTruthy = true;
+  arr.forEach(obj => {
+    var isKeyTruty = Boolean(obj[key]);
+    if (!isKeyTruty) {
+      isTruthy = false;
+    }
+  });
+  return isTruthy;
 }
-
-export {
-  truthCheck,
-};
+export { truthCheck };
