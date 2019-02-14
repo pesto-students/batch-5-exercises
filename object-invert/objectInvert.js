14603eef1031da1fa76da0bf654815b395cd3814
Kaushik Bokka <kaushikbokka@gmail.com>
2019-02-15 01:46:38 +0530

    Solve Exercise

--- object-invert/objectInvert.js
+++ object-invert/objectInvert.js
@@ -1,9 +1,9 @@
 
 function objectInvert(sampleObject) {
-  // const invertedObject = {};
-  // const invertFunction = key => invertedObject[sampleObject[key]] = key;
-  // Object.keys(sampleObject).map(invertFunction);
-  return sampleObject;
+  const invertedObject = {};
+  const invertFunction = key => invertedObject[sampleObject[key]] = key;
+  Object.keys(sampleObject).map(invertFunction);
+  return invertedObject;
 }
 
 export {
