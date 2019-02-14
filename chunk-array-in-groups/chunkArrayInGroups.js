6f9b818500929c299eb767c396b885977747809c
Ankit307 <bhagatsingh.solanki@appinventiv.com>
2019-02-15 00:24:22 +0530

    Solved Chunk array in group

--- chunk-array-in-groups/chunkArrayInGroups.js
+++ chunk-array-in-groups/chunkArrayInGroups.js
@@ -1,6 +1,17 @@
-
 function chunkArrayInGroups(...args) {
-  return args;
+  var elementsToGroup = args[0];
+  var sizeOfGroup = args[1];
+  elementsToGroup = elementsToGroup.reduce((accumulator, curruentValue, index) => {
+    var pushToIndex = accumulator.length > 0 ? accumulator.length - 1 : 0;
+
+    if (accumulator[pushToIndex] && accumulator[pushToIndex].length && accumulator[pushToIndex].length < sizeOfGroup) {
+      accumulator[pushToIndex].concat([curruentValue]);
+    } else {
+      accumulator[pushToIndex] = [].concat([curruentValue]);
+    }
+    return accumulator;
+  }, []);
+  return elementsToGroup;
 }
 
 export {
