eb39eded167607a7d8b151089292e71b6973c9f4
Mehul <Mehul>
2019-02-15 00:22:55 +0530

    WIP : day-4-segment-1, object-values

--- object-values/objectValues.js
+++ object-values/objectValues.js
@@ -1,8 +1,11 @@
-
-function objectValues(...args) {
-  return args;
+function objectValues(obj) {
+  var result = [];
+  for (var key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      result.push(obj[key]);
+    }
+  }
+  return result;
 }
 
-export {
-  objectValues,
-};
+export { objectValues };
