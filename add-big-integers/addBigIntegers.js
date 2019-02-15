fc8acfbb08ccfb65a52e042ead80e38a8482e520
Mehul <Mehul>
2019-02-15 23:59:23 +0530

    WIP : day-5-segment-1, Imp : alien-alphabet

--- add-big-integers/addBigIntegers.js
+++ add-big-integers/addBigIntegers.js
@@ -1,8 +1,8 @@
-
-function addBigIntegers(...args) {
-  return args;
+function addBigIntegers(str) {
+  str = str.split(' ');
+  var totalNumbers = str[0];
+  var numbers = str.slice(1);
+  return numbers.reduce((sum, num) => sum + Number(num), 0);
 }
 
-export {
-  addBigIntegers,
-};
+export { addBigIntegers };
--- alien-alphabet/alienAlphabet.js
+++ alien-alphabet/alienAlphabet.js
@@ -1,8 +1,10 @@
-
-function alienAlphabet(...args) {
-  return args;
+function alienAlphabet(words) {
+  var uniqueAlphabets = [];
+  var concatenatedWords = words.join('').split('');
+  concatenatedWords.forEach(char =>
+    !uniqueAlphabets.includes(char) ? uniqueAlphabets.push(char) : null
+  );
+  return uniqueAlphabets;
 }
 
-export {
-  alienAlphabet,
-};
+export { alienAlphabet };
