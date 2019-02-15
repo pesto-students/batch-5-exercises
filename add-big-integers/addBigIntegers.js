3577a8de380bcca9a56b0d878dce5a53b62ee7d4
Ankit307 <bhagatsingh.solanki@appinventiv.com>
2019-02-16 00:03:00 +0530

    Soved Add Big Integers

--- add-big-integers/addBigIntegers.js
+++ add-big-integers/addBigIntegers.js
@@ -1,6 +1,10 @@
-
-function addBigIntegers(...args) {
-  return args;
+function addBigIntegers(numberData) {
+  numberData = numberData.split('\n');
+  var no = numberData[0];
+  var numbers = numberData.slice(1);
+  return numbers.reduce((prevValue, currentValue) => {
+    return String(parseFloat(prevValue) + parseFloat(currentValue));
+  }, 0);
 }
 
 export {
