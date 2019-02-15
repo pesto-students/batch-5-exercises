424732c5d4b1a93a18fd4a29a05c7e0715e7c942
Ankit307 <bhagatsingh.solanki@appinventiv.com>
2019-02-16 00:37:26 +0530

    Soved parse ms

--- parse-ms/parseMs.js
+++ parse-ms/parseMs.js
@@ -1,6 +1,14 @@
+function parseMs(milisSeconds) {
+  milisSeconds = Math.abs(milisSeconds);
 
-function parseMs(...args) {
-  return args;
+  const formatedMiliSeconds = {
+    days: (milisSeconds / (24 * 60 * 60 * 1000)).toFixed(1),
+    hours: (milisSeconds / (60 * 60 * 1000)).toFixed(1),
+    minutes: (milisSeconds / (60 * 1000)).toFixed(1),
+    seconds: (milisSeconds / 1000).toFixed(1),
+    milliseconds: (milisSeconds).toFixed(1),
+  }
+  return formatedMiliSeconds;
 }
 
 export {
