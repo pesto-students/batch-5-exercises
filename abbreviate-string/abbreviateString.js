650e7e44c4a28e185e90801ed03dca43dd8b47cd
Mehul <Mehul>
2019-02-15 23:46:59 +0530

    WIP : day-4-segment-1, Imp : abreviate-string

--- abbreviate-string/abbreviateString.js
+++ abbreviate-string/abbreviateString.js
@@ -1,8 +1,14 @@
-
-function abbreviateString(...args) {
-  return args;
+function abbreviateString(input) {
+  var isString = typeof input === 'string' && /[A-Z]/.test(input);
+  if (isString) {
+    var str = input.split(' ');
+    var firstWord = str[0];
+    var lastWord = str[str.length - 1];
+    var abbreviatedString = firstWord + ' ' + lastWord[0].toUpperCase() + '.';
+    return abbreviatedString;
+  } else {
+    throw new Error('The input must be a string.');
+  }
 }
 
-export {
-  abbreviateString,
-};
+export { abbreviateString };
