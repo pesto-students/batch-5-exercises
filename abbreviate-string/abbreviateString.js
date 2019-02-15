b7089e4a22b88a09f40e2633d6f470c1dfa412b7
Shalini <shalinityagi64@gmail.com>
2019-02-15 23:51:02 +0530

    Implemented abbreviateString

--- abbreviate-string/abbreviateString.js
+++ abbreviate-string/abbreviateString.js
@@ -1,6 +1,20 @@
+function validString(value) {
+  return typeof value === 'string';
+}
 
-function abbreviateString(...args) {
-  return args;
+function getAbbreviateString(string) {
+  const words = string.split(' ');
+  const firstWord = words[0];
+  const lastWord = words[words.length - 1];
+  const lastWordFirstCharacter = lastWord.split('')[0];
+  return `${firstWord} ${lastWordFirstCharacter.toUpperCase()}.`;
+}
+
+function abbreviateString(string) {
+  if (!validString(string)) {
+    throw new Error('Is not a valid string');
+  }
+  return getAbbreviateString(string);
 }
 
 export {
