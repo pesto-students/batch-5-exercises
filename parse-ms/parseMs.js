a89b1ea6b385b07ba0b513f19b96ec31d025edb7
Kaushik Bokka <kaushikbokka@gmail.com>
2019-02-16 02:00:18 +0530

    Solve two exercises

--- parse-ms/parseMs.js
+++ parse-ms/parseMs.js
@@ -1,6 +1,29 @@
 
-function parseMs(...args) {
-  return args;
+function parseMs(value) {
+  let milliseconds = Math.abs(value);
+  let days, hours, minutes, seconds;
+  seconds = Math.floor(milliseconds / 1000);
+  minutes = Math.floor(seconds / 60);
+  seconds = seconds % 60;
+  hours = Math.floor(minutes / 60);
+  minutes = minutes %60 ;
+  days = Math.floor(hours / 24);
+  hours = hours % 24;
+  milliseconds = milliseconds % 1000;
+  const parsedDayTimeObj = {
+    days: days,
+    hours: hours,
+    minutes: minutes,
+    seconds: seconds,
+    milliseconds: milliseconds
+  }
+  if (value < 0) {
+    for (let key in parsedDayTimeObj) {
+      parsedDayTimeObj[key] *= -1;
+    }
+    return parsedDayTimeObj;
+  }
+  return parsedDayTimeObj;
 }
 
 export {
