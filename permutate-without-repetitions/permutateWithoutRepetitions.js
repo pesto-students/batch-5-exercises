297eddbd501bc4b96fda9b6f401a0dcb3e1107d6
Kaushik Bokka <kaushikbokka@gmail.com>
2019-02-16 05:34:22 +0530

    Solve one exercises

--- permutate-without-repetitions/permutateWithoutRepetitions.js
+++ permutate-without-repetitions/permutateWithoutRepetitions.js
@@ -1,6 +1,12 @@
 
-function permutateWithoutRepetitions(...args) {
-  return args;
+function permutateWithoutRepetitions(sampleArray) {
+  // const permuatatedArray = [];
+  // const permuteRecusiveFunc = function(array, acc = []) {
+  //   if (array.length === 0) {
+  //     permuatatedArray.push(acc)
+  //   }
+  //   permuatatedArray = sampleArray.map(
+  return sampleArray;
 }
 
 export {
