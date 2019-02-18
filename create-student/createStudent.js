8791e8384e56b8337dcdde829e7947009e127196
Mehul <Mehul>
2019-02-19 00:35:48 +0530

    WIP: day-6-segment-1, Imp: create-student

--- create-student/createStudent.js
+++ create-student/createStudent.js
@@ -1,6 +1,16 @@
 
-function createStudent(...args) {
-  return args;
+function createStudent({ likesES2015 = true, likesJavaScript = true } = {}) {
+  let message = 'The student ';
+  if (likesES2015 && likesJavaScript) {
+    message += 'likes JavaScript and ES2015';
+  } else if (likesES2015) {
+    message += 'likes ES2015!';
+  } else if (likesJavaScript) {
+    message += 'likes JavaScript!';
+  } else {
+    message += 'does not like much...';
+  }
+  return message;
 }
 
 export {
