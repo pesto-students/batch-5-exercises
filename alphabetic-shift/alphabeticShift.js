6cbed05e720b0925989759c51b7b5cf899fb71d8
Mehul <Mehul>
2019-02-19 04:43:30 +0530

    day-6-segment-2, Imp: alphabetic-shift

--- alphabetic-shift/alphabeticShift.js
+++ alphabetic-shift/alphabeticShift.js
@@ -1,6 +1,8 @@
 
-function alphabeticShift(...args) {
-  return args;
+const shiftCharByOne = ch => String.fromCharCode(ch.charCodeAt(0) + 1);
+function alphabeticShift(str) {
+  const charArr = str.split('');
+  return charArr.reduce((acc, ch) => acc + shiftCharByOne(ch), '');
 }
 
 export {
