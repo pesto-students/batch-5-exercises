8a79f67ff43e53780f6d23af4396ad814f7c6946
Mehul <Mehul>
2019-02-19 05:10:54 +0530

    day-6-segment-2, Imp: person

--- person/Person.js
+++ person/Person.js
@@ -1,8 +1,12 @@
+export class Person {
+  constructor(firstName, lastName, dateOfBirth) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dateOfBirth = dateOfBirth;
+  }
 
-function person(...args) {
-  return args;
+  addDobDigits() {
+    const dobDigits = this.dateOfBirth.split('/').join('');
+    return dobDigits.split('').reduce((sum, num) => sum + Number(num), 0);
+  }
 }
-
-export {
-  person,
-};
