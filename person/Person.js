39b27139b4dc7f862ee10030809e5829e083761f
Shalini <shalinityagi64@gmail.com>
2019-02-19 05:02:10 +0530

    Implemented person

--- person/Person.js
+++ person/Person.js
@@ -1,8 +1,20 @@
+class Person {
+  constructor(firstName, lastName, dateOfBirth) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dateOfBirth = dateOfBirth;
+  }
 
-function person(...args) {
-  return args;
+  static calculateSum(accumulator, value) {
+    const digits = value.split('');
+    const sumOfDigits = digits.reduce((acc, currentValue) => acc + Number(currentValue), 0);
+    return accumulator + sumOfDigits;
+  }
+
+  addDobDigits() {
+    const numbers = this.dateOfBirth.split('/');
+    return numbers.reduce(Person.calculateSum, 0);
+  }
 }
 
-export {
-  person,
-};
+export { Person };
