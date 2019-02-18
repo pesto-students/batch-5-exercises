a61157023ca1932100484a3619741be687acf513
Mehul <Mehul>
2019-02-19 00:01:07 +0530

    WIP: day-6-segment-1, Imp: place-in-middle

--- place-in-middle/placeInMiddle.js
+++ place-in-middle/placeInMiddle.js
@@ -1,6 +1,9 @@
 
-function placeInMiddle(...args) {
-  return args;
+function placeInMiddle(baseList, toPlaceList) {
+  const middleIndexOfBaseList = Math.floor(baseList.length / 2);
+  const firstHalf = baseList.slice(0, middleIndexOfBaseList);
+  const secondHalf = baseList.slice(middleIndexOfBaseList);
+  return [...firstHalf, ...toPlaceList, ...secondHalf];
 }
 
 export {
