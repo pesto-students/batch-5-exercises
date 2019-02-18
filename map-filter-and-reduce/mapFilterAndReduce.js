c99598deba5e692e8ead6dc6831657ebaff66ccc
Mehul <Mehul>
2019-02-19 00:10:31 +0530

    WIP: day-6-segment-1, Imp: map-filter-and-reduce

--- map-filter-and-reduce/mapFilterAndReduce.js
+++ map-filter-and-reduce/mapFilterAndReduce.js
@@ -1,6 +1,11 @@
 
-function mapFilterAndReduce(...args) {
-  return args;
+function mapFilterAndReduce(list) {
+  return list.filter(obj => obj.firstName.length < 5)
+    .reduce((resultObj, curObj) => {
+      const result = { ...resultObj };
+      result[curObj.firstName] = curObj.firstName.length;
+      return result;
+    }, {});
 }
 
 export {
