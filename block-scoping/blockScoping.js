b5e1d6b9d151912e0e8b069b6e1bb2e39bd28133
Shalini <shalinityagi64@gmail.com>
2019-02-18 23:49:22 +0530

    Implemeneted block-scoping

--- block-scoping/blockScoping.js
+++ block-scoping/blockScoping.js
@@ -1,6 +1,10 @@
 
-function blockScoping(...args) {
-  return args;
+function blockScoping(number) {
+  const callbacks = [];
+  for (let i = 0; i <= number; i += 1) {
+    callbacks.push(() => i);
+  }
+  return callbacks[number]();
 }
 
 export {
