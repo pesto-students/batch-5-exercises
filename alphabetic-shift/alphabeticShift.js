9b7e8c90f46765a4dbc5d610d131e7c972e07adf
Ankit307 <bhagatsingh.solanki@appinventiv.com>
2019-02-19 04:45:50 +0530

    Solved Alphabetic Shift and all promises

--- alphabetic-shift/alphabeticShift.js
+++ alphabetic-shift/alphabeticShift.js
@@ -1,7 +1,9 @@
 
-function alphabeticShift(...args) {
-  return args;
+function alphabeticShift(stringToShift) {
+  const wordShifter = word => String.fromCharCode(word.charCodeAt() + 1);
+  return [...stringToShift].map(wordShifter).join('');
 }
+
 
 export {
   alphabeticShift,
