6f25cb39ad56c6b1a466d7a7764247433829fbce
Shalini <shalinityagi64@gmail.com>
2019-02-20 00:05:15 +0530

    Implemented simple iterable

--- simple-iterable/simpleIterable.js
+++ simple-iterable/simpleIterable.js
@@ -1,6 +1,25 @@
 
-function simpleIterable(...args) {
-  return args;
+function simpleIterable() {
+  const iterable = {
+    [Symbol.iterator]() {
+      let value = 0;
+      let done = false;
+      return {
+        next() {
+          value += 1;
+          if (value > 5) {
+            done = true;
+            value = undefined;
+          }
+          return {
+            value,
+            done,
+          };
+        },
+      };
+    },
+  };
+  return iterable;
 }
 
 export {
