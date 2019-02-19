044e0ca810ba5cab7c437fc6d628fab051abf207
Kaushik Bokka <kaushikbokka@gmail.com>
2019-02-20 00:40:56 +0530

    Solve SimpleIterable

--- simple-iterable/simpleIterable.js
+++ simple-iterable/simpleIterable.js
@@ -1,23 +1,47 @@
-const simpleIterable = {
-  [Symbol.iterator]() {
-    let state = 0;
-    return {
-      next() {
-        state += 1;
-        if (state < 6) {
-          return {
-            value: 1,
-            done: false,
+
+function simpleIterable() {
+  return {
+    [Symbol.iterator]() {
+      let value = 0;
+      return {
+        next() {
+          value += 1;
+          if (value < 6) {
+            return {
+              value: value,
+              done: false,
+            }
           }
-        }
-        return {
-          value: 1,
-          done: false,
+          return {
+            value: undefined,
+            done: true,
+          }
         }
       }
     }
   }
 }
+
+// const simpleIterable = {
+//   [Symbol.iterator]() {
+//     let state = 0;
+//     return {
+//       next() {
+//         state += 1;
+//         if (state < 6) {
+//           return {
+//             value: 1,
+//             done: false,
+//           }
+//         }
+//         return {
+//           value: 1,
+//           done: false,
+//         }
+//       }
+//     }
+//   }
+// }
 
 export {
   simpleIterable,
