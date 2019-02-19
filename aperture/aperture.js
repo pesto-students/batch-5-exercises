935ee2e2c9518044a31262fef6d78bf7214f1930
Mehul <Mehul>
2019-02-20 05:13:56 +0530

    day-7-segment-2, Imp: aperture

--- aperture/aperture.js
+++ aperture/aperture.js
@@ -1,6 +1,13 @@
 
-function aperture(...args) {
-  return args;
+function aperture(times, list) {
+  const result = [];
+  for (let i = 0; i < list.length; i += 1) {
+    if (i + times <= list.length) {
+      const pairsInTimes = list.slice(i, i + times);
+      result.push(pairsInTimes);
+    }
+  }
+  return result;
 }
 
 export {
--- array-cube-root-to-json/arrayCubeRootToJson.js
+++ array-cube-root-to-json/arrayCubeRootToJson.js
@@ -1,6 +1,17 @@
 
-function arrayCubeRootToJson(...args) {
-  return args;
+function arrayCubeRootToJson(numbers) {
+  if (!Array.isArray(numbers)) {
+    throw new Error('Invalid input');
+  }
+  const notNumbers = numbers.filter(el => typeof el !== 'number');
+  if (notNumbers.length > 0) {
+    throw new Error('Invalid input');
+  }
+  return numbers.reduce((acc, num) => {
+    const result = acc;
+    result[num] = Math.cbrt(num);
+    return result;
+  }, {});
 }
 
 export {
