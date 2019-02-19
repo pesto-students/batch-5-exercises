d3273a2d368154145fffdb72da8eac762a69cf57
Shalini <shalinityagi64@gmail.com>
2019-02-20 00:39:10 +0530

    Implemented fibonacci-iter

--- fibonacci-iter/fibonacciIter.js
+++ fibonacci-iter/fibonacciIter.js
@@ -1,7 +1,23 @@
 
-function fibonacciIter(...args) {
-  return args;
-}
+
+const fibonacciIter = {
+  [Symbol.iterator]() {
+    let firstNum = 0;
+    let secondNum = 1;
+    return {
+      next() {
+        const sum = firstNum + secondNum;
+        firstNum = secondNum;
+        secondNum = sum;
+        return {
+          value: sum,
+          done: false,
+        };
+      },
+    };
+  },
+};
+
 
 export {
   fibonacciIter,
