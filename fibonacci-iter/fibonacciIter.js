753ffb8eb56a8c17f4730925ab21430e8ddc6899
Mehul <Mehul>
2019-02-20 00:42:09 +0530

    day-7-segment-1, Imp: fibonacci-iter

--- fibonacci-iter/fibonacciIter.js
+++ fibonacci-iter/fibonacciIter.js
@@ -1,26 +1,19 @@
-
-function fibonacciIter() {
-  return {
-    [Symbol.iterator]() {
-      let n1 = 1;
-      let n2 = 1;
-      let fib = n1 + n2;
-      return {
-        next() {
-          const result = {
-            value: fib,
-            done: false,
-          };
-          n1 = n2;
-          n2 = fib;
-          fib = n1 + n2;
-          return result;
-        },
-      };
-    },
-  };
-}
-
-export {
-  fibonacciIter,
+export const fibonacciIter = {
+  [Symbol.iterator]() {
+    let n1 = 0;
+    let n2 = 1;
+    let fib = n1 + n2;
+    return {
+      next() {
+        const result = {
+          value: fib,
+          done: false,
+        };
+        n1 = n2;
+        n2 = fib;
+        fib = n1 + n2;
+        return result;
+      },
+    };
+  },
 };
