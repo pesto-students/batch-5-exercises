4cd6526ca0f4290163b828c23e9e097f4985e2a2
Ankit307 <bhagatsingh.solanki@appinventiv.com>
2019-02-20 05:19:39 +0530

    Solved Animal sort

--- animal-sort/animalSort.js
+++ animal-sort/animalSort.js
@@ -1,6 +1,14 @@
 
-function animalSort(...args) {
-  return args;
+function animalSort(elements) {
+  const sortAnimalsbyNoOFLegs = (animalA, animalB) => {
+    const legs = animalA.numberOfLegs - animalB.numberOfLegs;
+    if (legs !== 0) {
+      return legs;
+    }
+    const name = animalA.name.charAt(0) - animalB.name.charAt(0);
+    return name;
+  };
+  return elements.sort(sortAnimalsbyNoOFLegs);
 }
 
 export {
