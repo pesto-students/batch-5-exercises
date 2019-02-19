57bfc89dde94a01483a83bc53e18afc18ab9a2de
Mehul <Mehul>
2019-02-19 23:51:33 +0530

    day-7-segment-1, Imp: simple-iterator

--- simple-iterable/simpleIterable.js
+++ simple-iterable/simpleIterable.js
@@ -1,6 +1,25 @@
 
-function simpleIterable(...args) {
-  return args;
+function simpleIterable() {
+  return {
+    [Symbol.iterator]() {
+      let state = 0;
+      return {
+        next() {
+          state += 1;
+          if (state <= 5) {
+            return {
+              value: state,
+              done: false,
+            };
+          }
+          return {
+            value: undefined,
+            done: true,
+          };
+        },
+      };
+    },
+  };
 }
 
 export {
