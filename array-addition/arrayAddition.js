90cb07a52f304d5db738ba5b5e64c2ff898df168
Mehul <Mehul>
2019-02-20 04:46:51 +0530

    day-7-segment-2, array-addition

--- array-addition/arrayAddition.js
+++ array-addition/arrayAddition.js
@@ -1,6 +1,20 @@
 
-function arrayAddition(...args) {
-  return args;
+function arrayAddition(arr1, arr2) {
+  if (!arr1.length || !arr2.length) {
+    throw new Error();
+  }
+  const result = [];
+  const length = Math.min(arr1.length, arr2.length);
+  let i = 0;
+  while (i < length) {
+    if (typeof arr1[i] === 'string' || typeof arr2[i] === 'string') {
+      throw new Error();
+    }
+    result.push(arr1[i] + arr2[i]);
+    i += 1;
+  }
+  const remainingArr = arr1.length > arr2.length ? arr1.slice(i) : arr2.slice(i);
+  return result.concat(remainingArr);
 }
 
 export {
