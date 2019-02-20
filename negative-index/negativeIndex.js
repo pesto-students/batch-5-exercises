f2d87ad7e0cb301d46744bed210210c31dc87af6
Kaushik Bokka <kaushikbokka@gmail.com>
2019-02-21 01:39:46 +0530

    wip

--- negative-index/negativeIndex.js
+++ negative-index/negativeIndex.js
@@ -1,6 +1,32 @@
+function get(targetArray, index) {
+  const reverseSupportedIndex = index + 3;
+  if (index >= 0) {
+    return targetArray[index];
+  }
+  return targetArray[reverseSupportedIndex];
+  }
 
-function negativeIndex(...args) {
-  return args;
+const handler = {
+  // get: function(targetArray, index) {
+
+  //   return targetArray[index];
+  // },
+  get: get,
+  set: function(targetArray, index, value) {
+    if(index >= 0) {
+      targetArray[index] = value;
+      return true;
+    } else {
+      const changedIndex = index + 3;
+      targetArray[changedIndex] = value;
+      return true;
+    }
+  }
+}
+
+function negativeIndex(sampleArray) {
+  const proxyArray = new Proxy(sampleArray, handler);
+  return proxyArray;
 }
 
 export {
