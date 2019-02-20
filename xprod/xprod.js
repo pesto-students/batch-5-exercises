1c137d1e0593385084ff4d093e6a8327aa73de04
Mehul <Mehul>
2019-02-21 00:25:23 +0530

    day-8-segment-1, Imp: xprod

--- xprod/xprod.js
+++ xprod/xprod.js
@@ -1,8 +1,10 @@
 
-function xprod(...args) {
-  return args;
+function xprod(arr1, arr2) {
+  return arr1
+    .map(el1 => arr2.map(el2 => [el1, el2]))
+    .reduce((result, curArr) => [...result, ...curArr], []);
 }
-
+// console.log(xprod([1, 2], ['a', 'b']));
 export {
   xprod,
 };
