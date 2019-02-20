e553db71156df0b80e9f6c2b9b929fd47c078713
Mehul <Mehul>
2019-02-21 00:07:33 +0530

    day-8-segment-1

--- negative-index/negativeIndex.js
+++ negative-index/negativeIndex.js
@@ -1,8 +1,18 @@
-
-function negativeIndex(...args) {
-  return args;
+const handler = {
+  get(arr, key) {
+    if (key === 'length') {
+      return arr.length;
+    }
+    // if (key < 0) {
+    //   return arr[arr.length] - 1;
+    // }
+    return arr;
+  },
+};
+function negativeIndex(arr) {
+  return new Proxy(arr, handler);
 }
-
+// console.log(negativeIndex([1, 2, 3, 4]).length);
 export {
   negativeIndex,
 };
--- set-default-property/setDefaultProperty.js
+++ set-default-property/setDefaultProperty.js
@@ -1,6 +1,15 @@
-
-function setDefaultProperty(...args) {
-  return args;
+function getHandler(defaultValue) {
+  return {
+    get(obj, key) {
+      if (key in obj) {
+        return obj[key];
+      }
+      return defaultValue;
+    },
+  };
+}
+function setDefaultProperty(obj, defaultValue) {
+  return new Proxy(obj, getHandler(defaultValue));
 }
 
 export {
