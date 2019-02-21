38361b205583a73ff5329caa6f2e30030fe01453
Kaushik Bokka <kaushikbokka@gmail.com>
2019-02-22 05:07:59 +0530

    Initial Commit

--- bizarre-string-incrementer/bizarreStringIncrementer.js
+++ bizarre-string-incrementer/bizarreStringIncrementer.js
@@ -1,6 +1,24 @@
 
-function bizarreStringIncrementer(...args) {
-  return args;
+function bizarreStringIncrementer(sampleString) {
+  const lastChar = sampleString.slice(-1);
+  if (isNaN(lastChar)) {
+    return `${sampleString}1`;
+  }
+  const arrayOfAll = sampleString.split('');
+  const arrayWithNumbers = arrayOfAll.filter(x => !(isNaN(x)));
+  const counts = [];
+  for (var i = 0; i < arrayWithNumbers.length; i++) {
+    var num = arrayWithNumbers[i];
+    counts[num] = counts[num] ? counts[num] + 1 : 1;
+  }
+  const numOfZeros = counts[0];
+  const numberInTheString = Number(arrayWithNumbers.slice(numOfZeros,).join(''));
+  const theStringZeros = Array(numOfZeros).fill(0).join('');
+  const arrayWithChars = arrayOfAll.slice(0, -(arrayWithNumbers.length));
+  if (numOfZeros > 1) {
+    return `${arrayWithChars.join('')}${theStringZeros}${numberInTheString+1}`;
+  }
+  return `${arrayWithChars.join('')}${numberInTheString+1}`;
 }
 
 export {
