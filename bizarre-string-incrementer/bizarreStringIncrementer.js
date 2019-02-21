92435b19b83628dffb2cfaa2ffeab7dd34dd0ab5
Ankit307 <bhagatsingh.solanki@appinventiv.com>
2019-02-22 05:09:24 +0530

    Solved bizarr-string,canvas,svg

--- bizarre-string-incrementer/bizarreStringIncrementer.js
+++ bizarre-string-incrementer/bizarreStringIncrementer.js
@@ -1,6 +1,10 @@
 
-function bizarreStringIncrementer(...args) {
-  return args;
+function bizarreStringIncrementer(string) {
+  const numberPattern = /\d+/g;
+  const matchString = /^[A-Za-z]+/;
+  const numberToInc = string.match(numberPattern);
+  const stringToInc = string.match(matchString);
+  return stringToInc[0] + (numberToInc.length > 0 ? parseInt(numberToInc, 10) + 1 : '');
 }
 
 export {
