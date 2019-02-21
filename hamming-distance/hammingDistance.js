62e1a8e0f8dfc1ff08b09adf37873397c950ae06
Mehul <Mehul>
2019-02-22 00:08:47 +0530

    day-9-segment-1

--- hamming-distance/hammingDistance.js
+++ hamming-distance/hammingDistance.js
@@ -1,6 +1,18 @@
-
-function hammingDistance(...args) {
-  return args;
+function hammingDistance(str1, str2) {
+  const equalLengths = str1.length === str2.length;
+  if (!equalLengths) {
+    throw new Error('Strings must be of equal lengths');
+  }
+  const str1Arr = str1.split('');
+  const str2Arr = str2.split('');
+  return str1Arr.reduce(
+    (result, ch, i) => {
+      if (ch === str2Arr[i]) {
+        return result;
+      }
+      return result + 1;
+    }, 0,
+  );
 }
 
 export {
--- primality-test/primalityTest.js
+++ primality-test/primalityTest.js
@@ -1,6 +1,22 @@
 
-function primalityTest(...args) {
-  return args;
+function primalityTest(n) {
+  if (n <= 1) {
+    return false;
+  }
+  if (n === 2 || n === 3) {
+    return true;
+  }
+  if (n % 2 === 0 || n % 3 === 0) {
+    return false;
+  }
+  let i = 5;
+  while (i <= Math.ceil(Math.sqrt(n))) {
+    if (n % i === 0) {
+      return false;
+    }
+    i += 2;
+  }
+  return true;
 }
 
 export {
