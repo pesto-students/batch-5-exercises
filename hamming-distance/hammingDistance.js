6c56d284f5fb69704207299b4ebbac8162784116
Shalini <shalinityagi64@gmail.com>
2019-02-22 00:49:42 +0530

    Implemented hamming distance

--- hamming-distance/hammingDistance.js
+++ hamming-distance/hammingDistance.js
@@ -1,6 +1,10 @@
 
-function hammingDistance(...args) {
-  return args;
+function hammingDistance(stringOne, stringTwo) {
+  if (stringOne.length !== stringTwo.length) {
+    throw new Error('String length must be equal');
+  }
+  const differenceArray = stringOne.split('').map((value, index) => (value === stringTwo[index] ? 0 : 1));
+  return differenceArray.reduce((acc, value) => acc + value, 0);
 }
 
 export {
