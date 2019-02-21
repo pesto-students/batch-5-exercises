9b4bbe42d57bef92159ac0cff3fe700d01e8db3e
Mohd Hassaan <sr.hassan23@gmail.com>
2019-02-22 05:01:13 +0530

    Test: Fix HTML video test

--- least-common-multiple/leastCommonMultiple.js
+++ least-common-multiple/leastCommonMultiple.js
@@ -1,7 +1,18 @@
 
-function leastCommonMultiple(...args) {
-  return args;
+const gcd = (num1, num2) => {
+  if (num2 === 0) {
+    return num1;
+  }
+  return gcd(num2, num1 % num2);
+};
+
+function leastCommonMultiple(num1, num2) {
+  if (num1 === 0 || num2 === 0) {
+    return 0;
+  }
+  return Math.abs((num1 * num2) / gcd(num1, num2));
 }
+
 
 export {
   leastCommonMultiple,
