e41444d6dc57f027605d7518e5e02e33bf3296a9
Mehul <Mehul>
2019-02-22 05:05:10 +0530

    day-9-segment-2

--- bizarre-string-incrementer/bizarreStringIncrementer.js
+++ bizarre-string-incrementer/bizarreStringIncrementer.js
@@ -1,8 +1,26 @@
+const appendLeadingZeros = (n, size) => {
+  let result = n.toString();
+  let i = n.length;
+  while (i < size) {
+    result = `0${result}`;
+    i += 1;
+  }
+  return result;
+};
+function bizarreStringIncrementer(str) {
+  const isStrNotEndingWithNumber = /.*[0-9]$/.test(str);
+  if (!isStrNotEndingWithNumber) {
+    return `${str}1`;
+  }
+  const numbersPresentInString = str.match(/([0-9]+)$/)[0];
+  const remainingStr = str.slice(0, str.indexOf(numbersPresentInString));
 
-function bizarreStringIncrementer(...args) {
-  return args;
+  return remainingStr + appendLeadingZeros(
+    (String(parseInt(numbersPresentInString, 10) + 1)),
+    numbersPresentInString.length,
+  );
 }
-
+// console.log(appendLeadingZeros('34', 4));
 export {
   bizarreStringIncrementer,
 };
