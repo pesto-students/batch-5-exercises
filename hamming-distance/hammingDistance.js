72e7abc36dd35dbfb33827eeaf4182f4a316cb0a
Ankit307 <bhagatsingh.solanki@appinventiv.com>
2019-02-22 02:32:32 +0530

    Solved Html form ,binary search

--- hamming-distance/hammingDistance.js
+++ hamming-distance/hammingDistance.js
@@ -1,7 +1,17 @@
 
-function hammingDistance(...args) {
-  
-  return args;
+
+function hammingDistance(input1, input2) {
+  if (input1.length !== input2.length) {
+    throw Error();
+  }
+  const diffElement = [...input1, input2].reduce((acc, curr) => {
+    const itemIndex = acc.indexOf(curr);
+    if (itemIndex === -1) {
+      return [...acc, curr];
+    }
+    return [...acc.slice(0, itemIndex), ...acc.slice(itemIndex + 1, acc.length)];
+  }, []);
+  return diffElement.length;
 }
 
 export {
