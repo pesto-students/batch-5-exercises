a017b727faceb7d421684e957fb386823620904d
Mehul <Mehul>
2019-02-21 23:23:40 +0530

    slice

--- slice/slice.js
+++ slice/slice.js
@@ -1,6 +1,19 @@
 
-function slice(...args) {
-  return args;
+function slice(start, end, input) {
+  if (start < 0 || end < 0 || start === end) {
+    return '';
+  }
+  let inp;
+  if (typeof input === 'string') {
+    inp = input.split('');
+  }
+  const result = inp.reduce((acc, el, i) => {
+    if (i >= start && i < end) {
+      return [...acc, inp[i]];
+    }
+    return acc;
+  }, []);
+  return typeof input === 'string' ? result.join('') : result;
 }
 
 export {
