89f62b81d56a589737b92ad0bc47dc0f24a371fc
Kaushik Bokka <kaushikbokka@gmail.com>
2019-02-22 00:45:19 +0530

    Solve Prime

--- primality-test/primalityTest.js
+++ primality-test/primalityTest.js
@@ -1,6 +1,17 @@
 
-function primalityTest(...args) {
-  return args;
+function primalityTest(number) {
+  if (number < 2) {
+    return false;
+  }
+  for(var i = 2; i < number; i++) {
+    if (number % i === 0) {
+      if (number === i) {
+        return true;
+      }
+      return false;
+    }
+  }
+  return true;
 }
 
 export {
