1aaf41f39bb8a65dd7dcff4efb9a468a75341d87
Mehul <Mehul>
2019-02-22 22:01:53 +0530

    day-6-segment-2,Imp: sleep-promise

--- sleep-promise/sleepPromise.js
+++ sleep-promise/sleepPromise.js
@@ -1,8 +1,15 @@
 
-function sleepPromise(...args) {
-  return args;
+function sleep(ms) {
+  const p = new Promise(res => setTimeout(res, ms));
+  function innerFn(value) {
+    return p.then(() => value);
+  }
+  innerFn.then = (...args) => p.then(...args);
+  innerFn.catch = Promise.resolve().catch;
+  // innerFn.then = () => p;
+  return innerFn;
 }
 
 export {
-  sleepPromise,
+  sleep,
 };
