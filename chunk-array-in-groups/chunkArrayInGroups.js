61298f9ffc56065992c98d2564e7f4fabacee9d9
Mehul <Mehul>
2019-02-24 06:08:22 +0530

    day-4-segment-1, Imp: chunk-array-in-groups

--- chunk-array-in-groups/chunkArrayInGroups.js
+++ chunk-array-in-groups/chunkArrayInGroups.js
@@ -1,8 +1,11 @@
-
-function chunkArrayInGroups(...args) {
-  return args;
+function chunkArrayInGroups(arr, size) {
+  const result = [];
+  let i = 0;
+  while (i < arr.length) {
+    result.push(arr.slice(i, i + size));
+    i += size;
+  }
+  return result;
 }
 
-export {
-  chunkArrayInGroups,
-};
+export { chunkArrayInGroups };
