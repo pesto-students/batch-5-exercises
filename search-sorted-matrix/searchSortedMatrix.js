0bc4a140a65547777931e3b5c4fa4f45e99f1d75
Mehul <Mehul>
2019-02-25 04:45:19 +0530

    day-8-segment-2, Imp: search-sorted-matrix

--- search-sorted-matrix/searchSortedMatrix.js
+++ search-sorted-matrix/searchSortedMatrix.js
@@ -1,7 +1,12 @@
 
 function searchSortedMatrix(obj) {
-  const { search, matrix } = obj.search;
-  return matrix.reduce((acc, arr) => Boolean(acc || arr.includes(search)), true);
+  const { search, matrix } = obj;
+  for (const arr of matrix) {
+    if (arr.includes(search)) {
+      return true;
+    }
+  }
+  return false;
 }
 
 export {
