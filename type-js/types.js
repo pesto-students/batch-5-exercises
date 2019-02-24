1f48c2bfb35904f81a570d2d6dd8b226ae7f2190
Mehul <Mehul>
2019-02-25 05:14:55 +0530

    day-8-segment-2, Imp: type-js

--- type-js/types.js
+++ type-js/types.js
@@ -1,6 +1,17 @@
 
-function type(...args) {
-  return args;
+function type(input) {
+  let result;
+  result = typeof input;
+  if (input === null) {
+    result = 'null';
+  }
+  if (/\/.*\//.test(input)) {
+    result = 'RegExp';
+  }
+  if (result === 'object' && Array.isArray(input)) {
+    result = 'array';
+  }
+  return result[0].toUpperCase() + result.slice(1);
 }
 
 export {
