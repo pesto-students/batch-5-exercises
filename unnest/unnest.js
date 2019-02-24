f457c329a2757b2d8b2163ba8c8e982019768bb4
Mehul <Mehul>
2019-02-25 05:02:00 +0530

    day-8-segment-2, Imp: unnest

--- unnest/unnest.js
+++ unnest/unnest.js
@@ -1,6 +1,21 @@
-
-function unnest(...args) {
-  return args;
+const isANumber = key => /^[0-9]$/gi.test(key);
+function unnest(input) {
+  let source;
+  if (!Array.isArray(input)) {
+    const validKeys = Object.keys(input).filter(key => isANumber(key));
+    source = validKeys.map(key => input[key]);
+  } else {
+    source = input.slice();
+  }
+  return source.reduce((acc, cur) => {
+    let result = [];
+    if (Array.isArray(cur)) {
+      result = cur;
+    } else {
+      result = [cur];
+    }
+    return acc.concat(result);
+  }, []);
 }
 
 export {
