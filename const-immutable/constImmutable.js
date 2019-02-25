fb78e33f4a9b94a16d23ad8c09f7542529901356
Mehul <Mehul>
2019-02-25 06:36:27 +0530

    day-6-segment-1, WIP: const-immutable

--- const-immutable/constImmutable.js
+++ const-immutable/constImmutable.js
@@ -1,6 +1,12 @@
 
-function constImmutable(...args) {
-  return args;
+function constImmutable() {
+  const account = {
+    username: 'pesto',
+    password: 'initialPassword',
+  };
+  Object.freeze(account);
+  account.password = 's3cret';
+  return account.password;
 }
 
 export {
