8c99408e42620f4e4c51e108b249587774337ef4
Mehul <Mehul>
2019-02-25 05:39:47 +0530

    day-8-segment-2, Imp: substitution-cipher

--- substitution-cipher/substitutionCipher.js
+++ substitution-cipher/substitutionCipher.js
@@ -1,17 +1,25 @@
-const isEven = n => n % 2 === 0;
-const shiftChar = (ch, i) => {
+const isEven = n => n % 2;
+const isAlphaCapital = code => (code >= 97 && code <= 122);
+const isAlphaSmall = code => (code >= 65 && code <= 90);
+const shiftBy = (chCode) => {
+  let result;
+  if (isAlphaCapital(chCode)) {
+    result = isEven(chCode - 96);
+  } else if (isAlphaSmall(chCode)) {
+    result = isEven(chCode - 64);
+  }
+  return result ? 4 : 6;
+};
+const shiftChar = (ch) => {
   const charCode = ch.charCodeAt(0);
-  if ((charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122)) {
+  if (isAlphaCapital(charCode) || isAlphaSmall(charCode)) {
     let newCharCode;
-    if (isEven(i)) {
-      newCharCode = charCode + 4;
-    } else {
-      newCharCode = charCode + 6;
-    }
-    if ((charCode >= 97 && charCode <= 122) && newCharCode > 122) {
+    newCharCode = charCode + shiftBy(charCode);
+
+    if (isAlphaCapital(charCode) && newCharCode > 122) {
       newCharCode = (newCharCode - 122) + 96;
     }
-    if ((charCode >= 65 && charCode <= 90) && newCharCode > 90) {
+    if (isAlphaSmall(charCode) && newCharCode > 90) {
       newCharCode = (newCharCode - 90) + 64;
     }
     return String.fromCharCode(newCharCode);
@@ -19,10 +27,10 @@ const shiftChar = (ch, i) => {
   return ch;
 };
 function substitutionCipher(str) {
-  const strWithoutSpaces = str.split('').join('');
-  return strWithoutSpaces.split('').reduce((acc, ch, i) => acc + shiftChar(ch, i), '');
+  return str.split('').reduce((acc, ch) => acc + shiftChar(ch), '');
 }
-
+// console.log(substitutionCipher('Aa Bb Cc Xx Yy Zz !?.\n'));
+// Ee Hh Gg Dd Cc Ff !?.\n
 export {
   substitutionCipher,
 };
