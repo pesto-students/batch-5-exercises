9a9ae18122a62fe95bb4374c7231068d34a3aad1
Mehul <Mehul>
2019-02-25 09:16:35 +0530

    day-6-segment-2, WIP: cycled, rangeIter

--- cycled/Cycled.js
+++ cycled/Cycled.js
@@ -1,8 +1,52 @@
+function iter(arr) {
+  return {
+    [Symbol.iterator]() {
+      let i = 0;
+      return {
+        next() {
+          i += 1;
+          if (i === arr.length) {
+            // return {
+            //   value: undefined,
+            //   done: true,
+            // };
+            i = 0;
+          }
+          const result = {
+            value: arr[i],
+            done: false,
+          };
+          return result;
+        },
+        current() {
+          return arr[i];
+        },
+        previous() {
+          i = i === 0 ? arr.length - 1 : i - 1;
+          return arr[i];
+        },
+      };
+    },
+  };
+}
+class Cycled {
+  constructor(arr) {
+    this.iter = iter(arr)[Symbol.iterator]();
+  }
 
-function cycled(...args) {
-  return args;
+  next() {
+    return this.iter.next().value;
+  }
+
+  current() {
+    return this.iter.current();
+  }
+
+  previous() {
+    return this.iter.previous();
+  }
 }
 
 export {
-  cycled,
+  Cycled,
 };
--- range-iter/rangeIter.js
+++ range-iter/rangeIter.js
@@ -1,8 +1,30 @@
-
-function rangeIter(...args) {
-  return args;
+function* rangeIter(start, end) {
+  if (start === undefined || end === undefined) {
+    // new TypeError('undefined is not a number');
+  }
+  let i = start;
+  while (i <= end) {
+    yield i;
+    i += 1;
+  }
+  // return {
+  //   [Symbol.iterator]() {
+  //     return {
+  //       next() {
+  //         if (start < end) {
+  //           const result = [];
+  //           let i = start;
+  //           while (i <= end) {
+  //             result.push(i);
+  //             i += 1;
+  //           }
+  //           return { value: 12, done: true };
+  //         }
+  //         return { value: undefined, done: true };
+  //       },
+  //     };
+  //   },
+  // };
 }
 
-export {
-  rangeIter,
-};
+export { rangeIter };
