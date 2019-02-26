c82a4f6df7efc62d4d9e23d3a634fdb15c3dc52f
Mehul <Mehul>
2019-02-27 00:34:31 +0530

    day 12 segment 1, Imp: does-end-with

--- does-end-with/doesEndWith.js
+++ does-end-with/doesEndWith.js
@@ -1,6 +1,8 @@
 
-function doesEndWith(...args) {
-  return args;
+function doesEndWith(test, input) {
+  const testInput = Array.isArray(test) ? test.join('') : test;
+  const str = Array.isArray(input) ? input.join('') : input;
+  return str.endsWith(testInput);
 }
 
 export {
