6cd04d26fb2efd7a2fcea69fc5f176029d5b4552
Shalini <shalinityagi64@gmail.com>
2019-03-01 04:49:02 +0530

    Completed setup-router

--- setup-router/src/App.js
+++ setup-router/src/App.js
@@ -1,21 +1,28 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import NotFound from './components/NotFound';
 
-import './styles/App.css';
+import "./styles/App.css";
 
 class App extends Component {
   render() {
     return (
-      <Fragment>
-        <Navbar />
-        <Home />
-        <About />
-        <Contact />
-      </Fragment>
+      <Router>
+        <div>
+          <Navbar />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/about/" component={About} />
+            <Route path="/contact/" component={Contact} />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
