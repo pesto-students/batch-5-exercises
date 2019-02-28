9b10e993aa94612773d21f53338f46654353055c
Mohd Hassaan <sr.hassan23@gmail.com>
2019-03-01 00:53:17 +0530

    Day 14 segment 1

--- setup-router/src/App.js
+++ setup-router/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component, Fragment } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+import NotFound from './components/NotFound';
 
 import './styles/App.css';
 
@@ -11,10 +13,17 @@ class App extends Component {
   render() {
     return (
       <Fragment>
-        <Navbar />
-        <Home />
-        <About />
-        <Contact />
+        <BrowserRouter>
+          <div className="App">
+            <Navbar />
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route path='/About' component={About} />
+              <Route path='/Contact' component={Contact} />
+              <Route path='/:any' component={NotFound} />
+            </Switch>
+          </div>
+        </BrowserRouter>
       </Fragment>
     );
   }
