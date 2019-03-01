c85ed6de1d86e728d6b2a97fa1d929f7285a1b98
Mohd Hassaan <sr.hassan23@gmail.com>
2019-03-02 02:39:24 +0530

    Day 15 Segment 1

--- test-basics/src/App.js
+++ test-basics/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 import './styles/App.css';
 
@@ -9,18 +9,39 @@ class App extends Component {
     input: '',
     mainColor: 'blue',
   }
+  handelClick = () => {
+    this.setState({ on: !this.state.on });
+  }
+
+  handleChange = (e) => {
+    console.log(e);
+    this.setState({ input: e.currentTarget.value });
+  }
+
+  handleStrings = (str) => {
+    return str.length !== 0;
+  }
+
   render() {
     return (
       <div className="App">
-        Welcome to React
+        <h1 className={this.state.mainColor}>Welcome to React</h1>
+        <button className="button-state" onClick={this.handelClick}>{this.state.on ? 'Yes!' : 'No!'}</button>
+        <h2>{this.state.input}</h2>
+        <input type="text" onChange={this.handleChange} value={this.state.input} />
       </div>
     );
   }
 }
 
 export class Link extends Component {
+
   render() {
-    return null;
+    if (this.props.hide) {
+      return null;
+    }
+    return (<a href={this.props.address || ''}>Click Me</a>);
+
   }
 }
 
