8f535ff92747754d9f0774b089893a4dbd926eb5
Mehul <Mehul>
2019-03-07 00:12:11 +0530

    day 18 segment 1, Imp: templating-engine

--- templating-engine/src/index.js
+++ templating-engine/src/index.js
@@ -4,10 +4,8 @@ const app = express();
 
 const languages = ['JavaScript', 'Haskell', 'Python'];
 
-app.get('/', (req, res) => {
-  return res.send(`Hello ${languages[0]}`); // Use res.render() to render the ejs file instead of sending text response
-});
+app.set('view engine', 'ejs');
 
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
-});
+app.get('/', (req, res) => res.render('index', { languages }));
+
+app.listen(3000, () => console.log('Server is listening on port 3000'));
