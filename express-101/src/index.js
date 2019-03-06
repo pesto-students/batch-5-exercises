daa526b20f52f474d0a68e5f5be8aaf19069e0e6
Mohd Hassaan <sr.hassan23@gmail.com>
2019-03-07 00:27:43 +0530

    Day 18 Segment 1

--- express-101/src/index.js
+++ express-101/src/index.js
@@ -2,8 +2,19 @@ const express = require('express');
 
 const app = express();
 
-// 1) Create Routes
+const router = express.Router();
 
-// 2) Start server on port 3000
+// 1) Create Routes
+router.get('/', function (req, res) {
+    res.end('Hey, I am server response');
+});
+
+router.get('/movie/:title', function (req, res) {
+    res.end(`My favorite movie is ${req.params.title}`);
+});
+
+app.use('/', router);
+
+app.listen(3000);
 
 module.exports = app;
