bc1e39fc1d54885e63c9472f2e748aa34aaef85a
Kaushik Bokka <kaushikbokka@gmail.com>
2019-03-07 00:48:47 +0530

    form

--- express-forms/src/index.js
+++ express-forms/src/index.js
@@ -1,6 +1,10 @@
-const express = require('express');
 
+const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   return res.redirect('/form-with-get');
@@ -13,6 +17,14 @@ app.get('/form-with-get', (req, res) => {
 app.get('/form-with-post', (req, res) => {
   return res.render('form-with-post');
 });
+
+app.get('/submit-form-with-get/', function(req, res) {
+  res.send(req.query);
+})
+
+app.post('/submit-form-with-post/', function(req, res) {
+  res.send(req.body);
+})
 
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
