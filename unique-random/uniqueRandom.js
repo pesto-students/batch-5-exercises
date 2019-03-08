a19aa5a004a146d6107a07f329c1a9cb98287253
Mohd Hassaan <sr.hassan23@gmail.com>
2019-03-09 00:39:00 +0530

    Day 20 Segment 1

--- unique-random/uniqueRandom.js
+++ unique-random/uniqueRandom.js
@@ -1,6 +1,17 @@
+function getRandom(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
 
-function uniqueRandom(...args) {
-  return args;
+function uniqueRandom(min, max) {
+  let prev = null;
+  return () => {
+    let current = getRandom(min, max);
+    while (prev === current) {
+      current = getRandom(min, max);
+    }
+    prev = current;
+    return current;
+  }
 }
 
 export {
